e964c22fd306396aa2ec7fdeab5f990f59764ddb
mntone <901816+mntone@users.noreply.github.com>
2023-06-19 18:50:50 +0900

    Improve container size styles.

--- clogs/scripts/app.js
+++ clogs/scripts/app.js
@@ -1,6 +1,9 @@
 import AppearanceModal from './components/AppearanceModal'
 import ModalSupport from './utils/ModalSupport'
 
+const scrollbarWidth = window.innerWidth - document.body.clientWidth
+document.documentElement.style.setProperty('--scrollbar-width', scrollbarWidth + 'px')
+
 new AppearanceModal()
 
 // Hook all modals if available
